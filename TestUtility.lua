8bf495869b60d44260b467c56f60f30a99bfe74b
ponpon <wizardsoloist1983@gmail.com>
2017-04-13 17:14:33 +0800

    add test of Utility:randomPopTable()

--- TestUtility.lua
+++ TestUtility.lua
@@ -3,3 +3,17 @@ local Utility = require("Utility")
 local point = Utility:getRandomPointInCircle( { x = 0, y = 0 }, 1 )
 
 print( "x = " .. point.x .. ", y = " .. point.y )
+
+local testInputTable = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+local testOutPutTable = {}
+
+while Utility:getTableSize( testInputTable ) > 0 do
+  local testResult = Utility:randomPopTable( testInputTable )
+
+  if testResult then
+    table.insert( testOutPutTable, testResult )
+  end
+
+  debug_print( "testInputTable = ", testInputTable )
+  debug_print( "testOutPutTable = ", testOutPutTable )
+end
